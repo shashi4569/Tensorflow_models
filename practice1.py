53d621183277d5e86edbde613e874fe3c78bfef0
shashi <shashireddy.job@gmail.com>
2018-03-26 23:46:10 -0500

    initial

--- practice1.py
+++ practice1.py
@@ -11,3 +11,4 @@ sess = tf.Session()
 File_Writer = tf.summary.FileWriter('C:/Users/shash/PycharmProjects/Tensorflow_models/graph', sess.graph)
 print(sess.run(c))
 sess.close()
+# more boilerplates are required for good practice
